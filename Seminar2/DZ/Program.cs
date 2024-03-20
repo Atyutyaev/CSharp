4a02e8d9b8fe2e07baf3c557cb3ba34006ba9f8d
Andrey Tyutyaev <atyutyaev@mail.ru>
2024-03-20 13:30:32 +0300

    add Task1

---
+++ Seminar2/DZ/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает 
+//на вход число и проверяет, кратно ли оно 
+//одновременно 7 и 23
+//a=50   => нет
+//a=7    => нет
+//a=322  => да
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result1 = num % 7;
+int result2 = num % 23;
+if (result1 == 0 && result2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
