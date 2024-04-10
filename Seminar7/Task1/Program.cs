dbb122008164f03bc64ce0a4d694b32d223cd525
Andrey Tyutyaev <atyutyaev@mail.ru>
2024-04-10 11:13:26 +0300

    add Seminar7

---
+++ Seminar7/Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в 
+// промежутке от M до N. Использовать рекурсию, 
+// не использовать циклы.
+
+void NaturalNumbersBetweenMN(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write($"{m}");
+        return;
+    }
+
+    Console.Write($"{m}, ");
+    NaturalNumbersBetweenMN(m + 1, n);
+}
+
+void NaturalNumbersBetweenNM(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write($"{m}");
+        return;
+    }
+
+    Console.Write($"{m}, ");
+    NaturalNumbersBetweenNM(m - 1, n);
+}
+
+void GenerateSequence(int m, int n)
+{
+    if (m < n)
+    {
+        NaturalNumbersBetweenMN(m, n);
+    }
+    else if (m > n)
+    {
+        NaturalNumbersBetweenNM(m, n);
+    }
+    else
+    {
+        Console.Write(n); 
+    }
+}
+
+Console.WriteLine("Введите первое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+GenerateSequence(m, n);
---
+++ Seminar7/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте произвольный массив. Выведете его элементы, 
+// начиная с конца. Использовать рекурсию, 
+// не использовать циклы.
+
+class Program
+{
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Произвольный массив
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+
+        Console.WriteLine("Элементы массива, начиная с конца:");
+        PrintArrayReverse(array, array.Length - 1);
+    }
+}
